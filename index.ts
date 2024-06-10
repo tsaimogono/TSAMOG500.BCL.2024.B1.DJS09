e355665ee12f48f361157082b1c06d5459fd4514
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-10 14:57:33 +0200

    This line checks if isLoyalty is true or false. If isLoyalty is true, iconDisplay is set to the string '⭐'. If isLoyalty is false, iconDisplay is set to an empty string

--- index.ts
+++ index.ts
@@ -25,10 +25,9 @@ const reviews = [
     },
 ]
 
-
-function showReviewTotal (value : number, reviewer: string) {
+function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + 
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
 }
 
-showReviewTotal(reviews.length, reviews[0].name)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
