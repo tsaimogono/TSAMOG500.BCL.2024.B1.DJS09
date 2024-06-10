3a99dec7431f2b016b822e848a00658e549b7164
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-10 15:09:42 +0200

    document.querySelector() to select HTML elements based on their IDs.

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
-const reviewTotalDisplay = document.querySelector('#reviews')
 
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+const reviewTotalDisplay = document.querySelector('#reviews')
 const reviews = [
     {
         name: 'Sheia',
