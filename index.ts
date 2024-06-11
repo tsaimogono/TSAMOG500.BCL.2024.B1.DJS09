331e0f1f65864165524bbe2e19df9434675d7217
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-11 07:22:49 +0200

    assinged stayat as string

--- index.ts
+++ index.ts
@@ -35,19 +35,22 @@ const you: {
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
    firstName: 'Bobby',
    lastName: 'Brown',
    isReturning: true,
    age: 35
+   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-function populateUser(isReturning : boolean, userName : string ) {
+
+function populateUser(isReturning : boolean, userName: string ) {
     if (isReturning){
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstName)
 
